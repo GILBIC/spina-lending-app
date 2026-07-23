042902e5c76761b09914d5df4f445312b3917f91
GILBIC <gilbicsanjose@gmail.com>
2026-07-23 20:24:21 +0800

    Prepare Cash Control UI controls module

---
+++ spina_app/ui_controls.py
@@ -0,0 +1,8 @@
+"""Small Tkinter form and Treeview helpers extracted from SPINA."""
+
+from __future__ import annotations
+
+import tkinter as tk
+from tkinter import ttk
+
+from spina_app.theme_palettes import _spina_v21_cash_colors
