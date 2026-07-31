7d9dbd77db181010b1b8737fbe5f1e544b6d4b30
GILBIC <gilbicsanjose@gmail.com>
2026-07-31 12:56:28 +0800

    Test SPINA role mapping

--- gilbic_mobile/test/app_role_test.dart
+++ gilbic_mobile/test/app_role_test.dart
@@ -2,14 +2,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:gilbic_mobile/src/core/auth/app_role.dart';
 
 void main() {
-  test('parses supported Gilbic roles', () {
+  test('parses supported Gilbic and SPINA roles', () {
     expect(AppRole.fromValue('Client'), AppRole.client);
     expect(AppRole.fromValue('collector'), AppRole.collector);
-    expect(AppRole.fromValue(' EMPLOYEE '), AppRole.employee);
-    expect(AppRole.fromValue('Management'), AppRole.management);
+    expect(AppRole.fromValue('Office Staff'), AppRole.employee);
+    expect(AppRole.fromValue('encoder'), AppRole.employee);
+    expect(AppRole.fromValue('Manager'), AppRole.management);
+    expect(AppRole.fromValue('Supervisor'), AppRole.management);
+    expect(AppRole.fromValue('system_admin'), AppRole.management);
   });
 
   test('rejects unknown roles', () {
-    expect(AppRole.fromValue('administrator'), isNull);
+    expect(AppRole.fromValue('temporary guest'), isNull);
   });
 }
