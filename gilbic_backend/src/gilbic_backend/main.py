691c988e39c0ab9492647014c33d368e041a6785
GILBIC <gilbicsanjose@gmail.com>
2026-08-03 09:56:52 +0800

    Register activity notification API

--- gilbic_backend/src/gilbic_backend/main.py
+++ gilbic_backend/src/gilbic_backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 
 from . import __version__
+from .activity_notification_api import create_activity_notification_router
 from .auth_api import create_auth_router
 from .collection_api import create_collection_api_router
 from .collection_correction_api import create_collection_correction_router
@@ -45,6 +46,7 @@ def create_app() -> FastAPI:
     app.include_router(create_collection_correction_router())
     app.include_router(create_remittance_router())
     app.include_router(create_notification_router())
+    app.include_router(create_activity_notification_router())
     app.include_router(create_remittance_photo_router())
     return app
 
