b84f71edf90ac341020489de1bc791fe10891a53
GILBIC <gilbicsanjose@gmail.com>
2026-08-11 23:53:10 +0800

    Accounting 5D.25: register greenfield EIR anchor router

--- gilbic_backend/src/gilbic_backend/main.py
+++ gilbic_backend/src/gilbic_backend/main.py
@@ -28,6 +28,9 @@ from .eir_period_journal_api import create_eir_period_journal_router
 from .financial_accounting_api import create_financial_accounting_router
 from .financial_statements_api import create_financial_statements_router
 from .general_journal_api import create_general_journal_router
+from .greenfield_regular_eir_anchor_api import (
+    create_greenfield_regular_eir_anchor_router,
+)
 from .loan_disbursement_cancellation_api import (
     create_loan_disbursement_cancellation_router,
 )
@@ -96,6 +99,7 @@ def create_app() -> FastAPI:
     app.include_router(create_loan_disbursement_journal_posting_router())
     app.include_router(create_loan_disbursement_cancellation_router())
     app.include_router(create_loan_renewal_execution_evidence_router())
+    app.include_router(create_greenfield_regular_eir_anchor_router())
     app.include_router(create_eir_cash_allocation_router())
     app.include_router(create_eir_period_journal_router())
     app.include_router(create_cross_period_accounting_sequence_router())
