1388cd1bd4e1cf270797190c83db8cf4b24bf4af
GILBIC <gilbicsanjose@gmail.com>
2026-08-15 22:08:02 +0800

    UI: keep CA4 review behind real authentication

--- gilbic_mobile/lib/main.dart
+++ gilbic_mobile/lib/main.dart
@@ -1,21 +1,7 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:gilbic_mobile/src/app.dart';
-import 'package:gilbic_mobile/src/features/collector/collector_synthetic_review_page.dart';
-import 'package:gilbic_mobile/src/theme/spina_theme.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  if (kDebugMode) {
-    runApp(
-      MaterialApp(
-        title: 'SPINA Collector CA4 Review',
-        debugShowCheckedModeBanner: false,
-        theme: SpinaTheme.light,
-        home: const CollectorSyntheticReviewPage(),
-      ),
-    );
-    return;
-  }
   runApp(const GilbicApp());
 }
