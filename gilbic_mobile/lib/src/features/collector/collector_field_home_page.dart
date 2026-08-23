29cd41e58f38bcbc8f312fec985a62d9b6aba10e
GILBIC <gilbicsanjose@gmail.com>
2026-08-24 07:28:17 +0800

    Mobile: open focused Give to Client handover queue

--- gilbic_mobile/lib/src/features/collector/collector_field_home_page.dart
+++ gilbic_mobile/lib/src/features/collector/collector_field_home_page.dart
@@ -8,6 +8,7 @@ import 'package:gilbic_mobile/src/core/payments/payment_submission_repository.da
 import 'package:gilbic_mobile/src/core/renewals/collector_renewal_workflow.dart';
 import 'package:gilbic_mobile/src/features/account/account_settings_page.dart';
 import 'package:gilbic_mobile/src/features/collector/collector_cash_status_card.dart';
+import 'package:gilbic_mobile/src/features/collector/collector_cash_to_client_page.dart';
 import 'package:gilbic_mobile/src/features/collector/collector_cash_to_receive_page.dart';
 import 'package:gilbic_mobile/src/features/collector/collector_master_review_page.dart';
 import 'package:gilbic_mobile/src/features/collector/collector_remittance_page.dart';
@@ -118,6 +119,20 @@ class _CollectorFieldHomePageState extends State<CollectorFieldHomePage> {
     }
     await _open(
       CollectorCashToReceivePage(
+        session: widget.session,
+        deviceIdentityProvider: widget.deviceIdentityProvider,
+      ),
+    );
+    _refreshCashStatus();
+  }
+
+  Future<void> _openCashToClient() async {
+    if (!widget.session.hasPermission('renewal.recommend.assigned')) {
+      _permissionMessage('assigned-client renewal cash handovers');
+      return;
+    }
+    await _open(
+      CollectorCashToClientPage(
         session: widget.session,
         deviceIdentityProvider: widget.deviceIdentityProvider,
       ),
@@ -360,6 +375,7 @@ class _CollectorFieldHomePageState extends State<CollectorFieldHomePage> {
               onOpenRemittance: _openRemittance,
               onOpenRenewals: _openRenewals,
               onOpenCashToReceive: _openCashToReceive,
+              onOpenCashToClient: _openCashToClient,
               onCashReleaseAlert: _showCashReleaseAlert,
             ),
           ),
