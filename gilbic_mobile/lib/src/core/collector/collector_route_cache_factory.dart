a1b46d2bd7144e5c5482df97160ff94197e54222
GILBIC <gilbicsanjose@gmail.com>
2026-09-03 07:06:37 +0800

    fix: avoid route cache platform alias shadowing

--- gilbic_mobile/lib/src/core/collector/collector_route_cache_factory.dart
+++ gilbic_mobile/lib/src/core/collector/collector_route_cache_factory.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:gilbic_mobile/src/core/collector/collector_route_cache.dart';
 import 'package:gilbic_mobile/src/core/collector/collector_route_cache_factory_stub.dart'
-    if (dart.library.io) 'collector_route_cache_factory_io.dart' as platform;
+    if (dart.library.io) 'collector_route_cache_factory_io.dart' as implementation;
 
 CollectorRouteCache createDefaultCollectorRouteCache({
   bool? isWeb,
   TargetPlatform? platform,
 }) {
-  return platform.createPlatformCollectorRouteCache(
+  return implementation.createPlatformCollectorRouteCache(
     isWeb: isWeb ?? kIsWeb,
     platform: platform ?? defaultTargetPlatform,
   );
