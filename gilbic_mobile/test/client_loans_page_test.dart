d780cfbc68ac20c86d499098a7aff3ae4b6e082a
GILBIC <gilbicsanjose@gmail.com>
2026-09-12 21:54:37 +0800

    test(priority-8): reject local Client loan progress

--- gilbic_mobile/test/client_loans_page_test.dart
+++ gilbic_mobile/test/client_loans_page_test.dart
@@ -33,6 +33,8 @@ void main() {
     expect(find.textContaining('Remaining: ₱4,950.00'), findsOneWidget);
     expect(find.text('Official remaining balance'), findsWidgets);
     expect(find.text('₱4,950.00'), findsWidgets);
+    expect(find.byType(LinearProgressIndicator), findsNothing);
+    expect(find.textContaining('% paid'), findsNothing);
 
     await tester.scrollUntilVisible(
       find.byKey(const Key('client-loan-seven-by-seven-loan')),
