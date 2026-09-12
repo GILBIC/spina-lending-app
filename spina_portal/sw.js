9da4a04ca2878bae1e79d6785a029bfd2b4b3d56
GILBIC <gilbicsanjose@gmail.com>
2026-09-12 09:39:02 +0800

    fix: precache Management financial statements module

--- spina_portal/sw.js
+++ spina_portal/sw.js
@@ -14,6 +14,7 @@ const SHELL_ASSETS = [
   '/assets/collector-contract.js',
   '/assets/staff-invite.js',
   '/assets/management-devices.js',
+  '/assets/management-financial-statements.js',
   '/assets/roles/client.js',
   '/assets/roles/employee.js',
   '/assets/roles/collector.js',
