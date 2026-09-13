66a54b14af646f28a241393b29b3c0a1fc617544
GILBIC <gilbicsanjose@gmail.com>
2026-09-13 11:06:57 +0800

    test: model pre-0118 7x7 replay schema

--- gilbic_backend/tests/test_seven_by_seven_no_collection_interest.py
+++ gilbic_backend/tests/test_seven_by_seven_no_collection_interest.py
@@ -13,8 +13,13 @@ class _Cursor:
     def __init__(self) -> None:
         self.query = ""
 
-    def execute(self, query: str, params) -> None:
+    def execute(self, query: str, params=()) -> None:
         self.query = query
+
+    def fetchone(self):
+        if "to_regclass('lending.seven_by_seven_penalty_payment_allocations')" in self.query:
+            return (None,)
+        return None
 
     def fetchall(self):
         if "from lending.collection_transactions transaction" in self.query:
